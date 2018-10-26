6a8a8f442ce59d73d2ebca2aba8d1e2489d4c23c
Adam Urbina <adamurbina@gmail.com>
2018-10-26 13:00:59 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,6 +22,12 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    
+  end
+
   def index
   end
 
