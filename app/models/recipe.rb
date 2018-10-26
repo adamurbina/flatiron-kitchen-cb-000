6b8470f21479ad9f06b6eb10b1929bdb8918f5a7
Adam Urbina <adamurbina@gmail.com>
2018-10-26 13:35:26 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -13,7 +13,7 @@ class Recipe < ActiveRecord::Base
     matches = self.all.select do |recipe|
       (ingredient_ids - recipe.all_ingredient_ids).empty?
     end
-    matches.delete_if{|r| r == recipe}
+    matches.delete_if{|r| r.id == recipe.id}
   end
 
 end
