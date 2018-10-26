01ba0984e4e9809435066dc562df0fc17a08c43f
Adam Urbina <adamurbina@gmail.com>
2018-10-26 12:26:29 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,6 +15,6 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:name, :ingredients => [])
+    params.require(:recipe).permit(:name, :ingredients => [:ingredients_ids])
   end
 end
