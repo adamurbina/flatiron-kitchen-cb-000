0eb08a15f599569daa7ac879c64a76d958ed3998
Adam Urbina <adamurbina@gmail.com>
2018-10-26 10:30:23 +0000

    automatically backed up by learn

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,20 @@
+class IngredientsController < ApplicationController
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.find_or_create_by(params[:name])
+    redirect_to @ingredient
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def index
+  end
+end
---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,11 @@
+class RecipesController < ApplicationController
+  def new
+    @recipe = Recipe.new
+  end
+
+  def edit
+  end
+
+  def index
+  end
+end
